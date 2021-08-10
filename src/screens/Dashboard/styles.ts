746ccf73b1fd26b3c057717c99b7c9262e6578f0
jvmMachado <joaovmmachado@gmail.com>
2021-08-09 23:41:11 -0300

    add styled-components and create first component

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #999;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
+  width: 100%;
+  text-align: center;
+`;
