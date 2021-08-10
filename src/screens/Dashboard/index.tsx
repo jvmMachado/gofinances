7edb640ae7941e8d6eb74e69a3a21da7c6c9ca9f
jvmMachado <joaovmmachado@gmail.com>
2021-08-10 02:55:57 -0300

    finish header's user info styles

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/26936266?v=4' }}/>
+            <User>
+              <UserGreeting style={{includeFontPadding: false}} >Olá,</UserGreeting>
+              <UserName style={{includeFontPadding: false}}>João</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
